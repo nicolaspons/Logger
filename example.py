44f3cb356728e21971253566f92f7b6cbcbc5e63
Nicolas Pons <nicolas.pons96@gmail.com>
2020-04-05 20:03:39 +0200

    example.py: used for testing purpose

---
+++ example.py
@@ -0,0 +1,14 @@
+from Logger import Logger
+import time
+import sys
+from multiprocessing import Pool
+
+
+def trainingUpdate(n, loss, dice):
+
+
+if __name__ == '__main__':
+    log = Logger()
+    log('hey')
+    time.sleep(3)
+    log()
