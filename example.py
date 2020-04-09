ca1a91f468c7eefe15d5ce6230ef40c320fc251c
Nicolas Pons <nicolas.pons96@gmail.com>
2020-04-09 17:34:59 +0200

    example.py: test script done

--- example.py
+++ example.py
@@ -1,14 +1,31 @@
-from Logger import Logger
+from Logger import TrainLogger
 import time
-import sys
-from multiprocessing import Pool
+import numpy as np
 
 
-def trainingUpdate(n, loss, dice):
+def load(filename: str, arg1: int, arg2: float):
+    time.sleep(2)
+
+
+def save(l):
+    time.sleep(2)
+
+
+def get_metrics():
+    return {'loss': np.random.randint(0, 10), 'dice': np.random.rand(
+    ), 'val_loss': np.random.randint(0, 10), 'val_dice': np.random.rand()}
+
+
+def train():
+    log('Loading samples', target=load, args=('some_file.png', 42, 10.0,))
+    time.sleep(1)
+    log.init_train(50, 10)
+    for _ in range(10):
+        time.sleep(np.random.randint(1, 5))
+        log.on_epoch_end(get_metrics())
+    log('Saving metrics', target=save, args=([1, 2, 3],))
 
 
 if __name__ == '__main__':
-    log = Logger()
-    log('hey')
-    time.sleep(3)
-    log()
+    log = TrainLogger(nb_epochs=10)
+    train()
